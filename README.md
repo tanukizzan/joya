cead46cda568f4f8d7258b27d95c1be7892c1304
tanukizzan <51444253+tanukizzan@users.noreply.github.com>
2020-12-31 17:14:24 +0900

    完成

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# joya
+# 除夜の鐘
+
+ネットで除夜の鐘を撞くことができるサイトです。思いついてから3時間くらいで作りました。108回撞くと何かが起きます。
+
+鐘の画像：https://www.irasutoya.com/2012/11/blog-post_5687.html  
+鐘の音声：https://dova-s.jp/se/play1136.html
---
+++ script.js
@@ -0,0 +1,42 @@
+const time = document.getElementById('time');
+const kane = document.getElementById('kane');
+const count = document.getElementById('count');
+const sound = document.getElementById('kane-sound');
+const result = document.getElementById('result');
+
+// 時刻表示
+function set2fig(num) {
+  var ret;
+  if(num < 10) {
+    ret = '0' + num;
+  } else {
+    ret = num;
+  }
+  return ret;
+}
+function clock() {
+  const now = new Date();
+  const youbi = ['日', '月', '火', '水', '木', '金', '土'];
+  time.innerHTML = now.getFullYear() + '年 ' + (set2fig(now.getMonth()) + 1) + '月' + set2fig(now.getDate()) + '日' + youbi[now.getDay()] + '曜日 ' + set2fig(now.getHours()) + '時' + set2fig(now.getMinutes()) + '分' + set2fig(now.getSeconds()) + '秒';
+}
+setInterval('clock()',1000);
+
+// 鐘をクリックしたら音声再生・アニメーション開始
+kane.addEventListener('click', function() {
+  sound.play();
+  kane.classList.add('animation');
+})
+
+let countValue = 0;
+// 鐘の音声が止まったらカウントアップ・アニメーション停止
+sound.addEventListener('ended', function() {
+  countValue++;
+  count.innerHTML = countValue;
+  if(countValue === 108) {
+    // 108回鳴らしたらハッピーニューイヤー
+    result.innerHTML = 'HAPPY NEW YEAR!';
+    result.style.color = 'red';
+    result.style.fontSize = '32px';
+  }
+  kane.classList.remove('animation');
+})
